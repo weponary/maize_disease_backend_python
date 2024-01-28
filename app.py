095acc7307cfaacc3eed3d3d81b22b627fdb4e1d
soshal dahal <soshal.dahal@kotuko.it>
2024-01-28 13:01:33 +0545

    fix: app fixed

--- app.py
+++ app.py
@@ -1,17 +1,58 @@
-# Use an official TensorFlow runtime as a parent image
-FROM tensorflow/tensorflow:latest
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+import tensorflow as tf
+import os
+import uuid
 
-# Set the working directory to /app
-WORKDIR /app
+app = Flask(__name__)
 
-# Copy the current directory contents into the container at /app
-COPY . /app
+# Load the pre-trained model and class names
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
 
-# Make port 5000 available to the world outside this container
-EXPOSE 5000
+loaded_model = tf.keras.models.load_model("/Users/soshaldahal/Documents/mazie_disease")
+class_names = ['Corn___Common_Rust', 'Corn___Gray_Leaf_Spot', 'Corn___Healthy', 'Corn___Northern_Leaf_Blight']
 
-# Run app.py when the container launches
-CMD ["python", "app.py"]
+uploads_folder = "uploads"
+uploads_path = os.path.join(current_dir, uploads_folder)
+
+
+
+# Create the uploads folder if it doesn't exist
+if not os.path.exists(uploads_path):
+    os.makedirs(uploads_path)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        image_file = data.get('image')       
+        image_path = os.path.join(uploads_path, image_file)
+
+        
+
+        if not os.path.exists(image_path):
+            return jsonify({'error': 'File not found'})
+
+        maize_image = cv2.imread(image_path)
+        maize_image = cv2.resize(maize_image, (299, 299))
+        maize_image = tf.expand_dims(maize_image, 0)
+
+        # Make prediction using the loaded model
+        prediction = loaded_model.predict(maize_image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+
+        result = {
+            'predicted_class': class_name,
+            'confidence': float(prediction[0][index]),
+        }
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
